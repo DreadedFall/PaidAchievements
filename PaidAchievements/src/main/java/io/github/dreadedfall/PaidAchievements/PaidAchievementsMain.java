b53eee811397a18bfaff963d19a30cec9c6ab1da
dreadedfall <dreadedfall@gmail.com>
2016-05-03 14:07:32 -0400

    basic edits

--- PaidAchievements/src/main/java/io/github/dreadedfall/PaidAchievements/PaidAchievementsMain.java
+++ PaidAchievements/src/main/java/io/github/dreadedfall/PaidAchievements/PaidAchievementsMain.java
@@ -1,8 +1,24 @@
 package io.github.dreadedfall.PaidAchievements;
 
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import net.milkbowl.vault.economy.Economy;
 
 public class PaidAchievementsMain extends JavaPlugin
 {
-
+	private Economy econ = null;
+	
+	public boolean setupEcon()
+	{
+		if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+	}
 }
